b06da0e56fb08c5ac3b21c122f925d2da7e6d874
ci-build <crn.poldracklab@gmail.com>
2020-04-08 01:08:18 +0000

    doc(update) [skip ci]

--- 0.4.2/_static/documentation_options.js
+++ 0.4.2/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.2+3.gb4d1290',
+    VERSION: '0.6.0rc1+1.g2f677e7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
